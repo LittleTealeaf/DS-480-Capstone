4e25b2e5338650240afe93a4909bea605330ee1e
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-03-28 09:43:23 -0400

    update target

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ agent = Agent([500, 500, 500, 500])
 
 for i in range(100):
     print("iter ", i)
+    if i % 10 == 0:
+        agent.update_target()
     agent.populate_replay(1000)
     agent.train(500)
     print(agent.evaluate(10))

4e4c251f20c91f5e904704f154c805aad98fd588
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-03-29 13:17:35 -0400

    added estimated time left

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from agents import Agent, ExpAgent
+import time
 
 
 agent = Agent(
@@ -9,15 +10,40 @@ agent = Agent(
     updates_per_step=1,
 )
 
+ITERATIONS = 10_000
+
 data = []
+elapsed_time = 0
+completed_iterations = 0
+TOTAL_ITERATIONS = ITERATIONS * 2
+
 
 for model in ["control", "experimental"]:
-    for i in range(10_000):
+    for i in range(ITERATIONS):
+
+        start = time.time()
+
         agent.populate_replay(100)
         agent.train(100)
         ev, freq = agent.evaluate(50)
         data.append([model, i, ev, freq[0], freq[1], freq[2], freq[3]])
-        print("Iter ", i, " ", ev, " ", freq)
+
+        end = time.time()
+
+        elapsed = end - start
+        elapsed_time += elapsed
+        completed_iterations += 1
+
+        avg_time = elapsed_time / completed_iterations
+        estimated_left = (TOTAL_ITERATIONS - completed_iterations) * avg_time
+        hours = estimated_left // 3600
+        minutes = (estimated_left % 3600) // 60
+        seconds = estimated_left % 60
+
+        # print("Iter ", i, " ", ev, " ", freq, "\t", f"Est Left: {hours}h {minutes}m {seconds}s")
+        print(
+                "Iter {}, ({}) {} \t Est. Left: {}h {}m {:.2f}s".format(i, ev, freq, hours, minutes, seconds)
+                )
 
     agent = agent.as_exp_agent()
 

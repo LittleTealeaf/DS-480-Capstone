cdf4416ab276745930532f852f001074ddfbf3b8
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-03-29 12:39:33 -0400

    changes

--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@ from agents import Agent, ExpAgent
 
 agent = ExpAgent(
     [1000, 1000, 1000, 1000],
-    width=4,
-    height=4,
+    width=5,
+    height=5,
     target_update_frequency=25,
     updates_per_step=4,
 )
 
 for i in range(10_000):
     agent.populate_replay(100)
-    agent.train(10)
-    print("Iter ", i, " ", agent.evaluate(100))
+    agent.train(100)
+    print("Iter ", i, " ", agent.evaluate(10))
 
     if agent.nan_check():
         print("HIT NAN")

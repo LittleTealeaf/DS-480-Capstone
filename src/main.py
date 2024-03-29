d3b0c08a8d75b095b49703ec3f4875a0d0b61e8f
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-03-29 10:35:44 -0400

    changes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from agents import Agent, ExpAgent
 
-agent = ExpAgent([1000, 1000, 1000, 1000, 1000], width=5, height=5)
+agent = ExpAgent([500, 500], width=3, height=3, target_update_frequency=100)
 
 for i in range(10_000):
-    agent.populate_replay(1000)
-    agent.train(500)
-    print(agent.evaluate(10))
+    agent.populate_replay(50)
+    agent.train(100)
+    print("Iter ", i, " ", agent.evaluate(10))

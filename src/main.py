2a1cf0c1e3a007c1523cb80cab3e3d00c24fba69
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-03-29 12:05:09 -0400

    changed dimensions and replay/train size

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from agents import Agent, ExpAgent
 
-agent = ExpAgent([1000, 1000], width=3, height=3, target_update_frequency=100)
+agent = ExpAgent([1000, 1000], width=2, height=2, target_update_frequency=100)
 
 for i in range(10_000):
-    agent.populate_replay(50)
-    agent.train(100)
+    agent.populate_replay(100)
+    agent.train(500)
     print("Iter ", i, " ", agent.evaluate(10))
 
     if agent.nan_check():
